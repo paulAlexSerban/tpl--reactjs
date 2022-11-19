1b95d36a31ecafcb1807d6620594218c86b0244b
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-19 02:53:02 +0200

    ci: add cd scripts

---
+++ scripts/deploy/package.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+echo "📦  Package Frontend Nginx"
+mkdir -p ../../package/reactjs
+cp -rfv ../../frontend/reactjs/build/* ../package/reactjs
